504f417ba2c70826d3ef60a6cc81cc25f740704f
David Catuhe <david.catuhe@live.fr>
2016-01-07 10:03:04 -0800

    fixing runtime generation

---
+++ dist/vorlonBrowserExtension.runtime.js
@@ -0,0 +1,9 @@
+var VBE;
+(function (VBE) {
+    var BestPractices;
+    (function (BestPractices) {
+        class Plugin {
+        }
+        BestPractices.Plugin = Plugin;
+    })(BestPractices = VBE.BestPractices || (VBE.BestPractices = {}));
+})(VBE || (VBE = {}));
